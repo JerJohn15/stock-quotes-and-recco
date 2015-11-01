7ada765914010a256f450981b4945f3236e1e98c
Matt Rkiouak <rkiouak@me.com>
2015-11-01 13:47:14 -0500

    fixed hard coded 'APPL' in filter method of results

--- server.js
+++ server.js
@@ -9,19 +9,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:symbol', function (req, res) {
-  console.log("in symbol path");
   var symbol=req.params.symbol;
-  console.log("symbol: ", symbol);
-  console.log('http://finance.yahoo.com/webservice/v1/symbols/'+symbol+'/quote?format=json&view=detail');
 var options = {
   uri: 'http://finance.yahoo.com/webservice/v1/symbols/'+symbol+'/quote?format=json&view=detail',
   json:true
 };
   requestPromise(options)
   .then(function (resp){
-
+    //console.log("resp: ", JSON.stringify(resp));
 var relevantStockResources = resp.list.resources.filter(function(resource){
-  return resource.resource.fields.symbol=="AAPL";}).map(function(resource){
+  return resource.resource.fields.symbol==symbol;}).map(function(resource){
     return {symbol:resource.resource.fields.symbol, price:resource.resource.fields.price, issuer:resource.resource.fields.issuer_name};
   })
 
