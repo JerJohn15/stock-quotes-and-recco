887bdc004e37a754e4e1eb417f4680534e3cd8c2
JerJohn15 <tjon2013@bu.edu>
2015-12-19 09:38:08 -0500

    Updated Exchange page
    Note: This section still isn't fixed yet. Added "states" to both input
    fields and drop down menu.

--- src/js/components/ExchangeRates.js
+++ src/js/components/ExchangeRates.js
@@ -10,22 +10,61 @@ US currency to foreign currency (vice-versa).
 import React from 'react';
 import NavigationBar from './NavigationBar';
 var fx = require("money");
+var accounting = require("accounting");
+// CSS for Exchange Rates page
+const styles = {}
+
+styles.body = {
+
+  backgroundcolor: '#00ff00'
+}
+
+styles.title  = {
+
+  padding: '20px',
+  paddingTop:'10px',
+  fontFamily:'Abril Fatface'
+}
 
 
-var body = "Below is a currency converter, converting foreign currency to US dollars. (vice-versa)";
+
+//Set the base currency and conversion
 fx.base ="USD";
 fx.settings = {
   from: "USD"
 };
+fx.rates = {
 
+"USDEUR" : 0.9202,
+"USDGBP" : 0.6715,
+"USDJPY" : 121.1400,
+"USD" : 1
+
+}
+// currency account settings
+accounting.settings = {
+currency: {
+  symbol: "$",
+  format: "$s%v",
+  precision: 2
+
+},
+number: {
+  precision: 0,
+  decimal: "."
+}
+
+};
 
 var ExchangeRates = React.createClass({
-//setup the base and get the current exchange rates here.
 
- function getCurrency(){
+
+ accessAPI: function (){
   $.getJSON(
-        //  using Open Exchange Rates here, but you can use any source!
-          'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=USDINR=X',
+        //  using yahoo finance api
+
+      //  'http://openexchangerates.org/latest.json',
+         'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=USDINR=X',
           function(data) {
               // Check money.js has finished loading:
               if ( typeof fx !== "undefined" && fx.rates ) {
@@ -42,15 +81,25 @@ var ExchangeRates = React.createClass({
           }
       );
 },
-function calculateRates(currencyType){
+getInitialState: function(){
+  return {moneyValue: ' ',
+        currencyType: ' ',
+        convertedCurrency: ' '};
+},
 
-var amount = document.getElementById("money").value;
+//sets the amount
+setAmount: function(event){
+   this.setState({moneyValue: event.target.value});
+},
 
-if(amount){
-  var input = document.getElementById(currencyType).value;
-
-  this.currency = fx.convert(amount, {to: input});//public variable accessed outside of function
-
+// Check if amount has been inputted by user, and get the converted currency. Otherwise, show error.
+calculateRates: function (event){
+if(this.state.moneyValue){//check if amount has been inputed by user
+//  this.amount = accounting.formatNumber(this.amount);
+//alert(this.state.moneyValue);
+   this.setState({currencyType: event.target.value});
+  var currency = fx.convert(this.state.moneyValue, {to: this.state.currencyType});
+  this.setState({convertedCurrency: currency});//sets converted currency
 }else{
   alert("Please enter a value!");
 }
@@ -59,31 +108,43 @@ if(amount){
 
 render: function(){
 
+var description = "Below is a currency converter, converting foreign currency to US dollars. (vice-versa)";
+
+var currency = this.state.convertedCurrency;
+
 
 return (
   <div>
 <NavigationBar/>
-  <p> {body}</p>
     <br>
     </br>
   <div>
   <form>
     <fieldset>
-      <legend>Currency Exchange</legend>
-      <label title = "Enter Price Here"> </label>
-    <input  type = "text" id = "money" value = "1"></input>
-    <br></br>
-      <select id ="selection" onChange={this.calculateRates(this.value)}>
+      <legend styles = {styles.title}>Currency Exchange</legend>
+        <br>
+        </br>
+        <p> {description}</p>
+      <label className = "Enter Price Here"> </label>
+    <input  type = "text" id = "money"
+      defaultValue = {this.state.moneyValue} onChange = {this.setAmount}>
+    </input>
+    <br>
+    </br>
+      <select id ="selection" defaultValue =
+        {this.state.currencyType} onChange={this.calculateRates}>
           <option value = "">select currency</option>
-      <option value = "EUR">US-Euro</option>
-          <option value = "GBP">US-Pound</option>
-              <option value = "JPY">US-Yuan</option>
+      <option value = "USDEUR">US-Euro</option>
+          <option value = "USDGBP">US-Pound</option>
+              <option value = "USDJPY">US-Yuan</option>
                   </select>
-        <br></br>
+        <br>
+        </br>
 
-<input type ="text" id = "foriegn" value = {this.currency}></input>
+<input type ="text" id = "foriegn" defaultValue = {currency}></input>
 
-        <br></br>
+        <br>
+        </br>
 
     </fieldset>
   </form>
